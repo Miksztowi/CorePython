d6c7fdbf4da9e72478cc95fa88ed48b88105c5ab
Wukong <binwengan@gmail.com>
2017-12-12 23:48:31 +0800

    Update blog files.

--- Fluent_Python_demo/Coroutines_demo/yield_from_pseudocode_demo.py
+++ Fluent_Python_demo/Coroutines_demo/yield_from_pseudocode_demo.py
@@ -18,45 +18,45 @@
 #             _r = _e.value
 #             break
 
-import sys
-
-RESULT = yield from EXPR
-_i = iter(EXPR)
-try:
-    _y = next(_i)
-except StopIteration as _e:
-    _r = _e.value
-else:
-    while 1:
-        try:
-            _s = yield _y
-        except GeneratorExit as _e:
-            try:
-                _m = _i.close
-            except AttributeError:
-                pass
-            else:
-                _m()
-            raise _e
-        except BaseException as _e:
-            _x = sys.exc_info()
-            try:
-                _m = _i.throw
-            except AttributeError:
-                raise _e
-            else:
-                try:
-                    _y = _m(*_x)
-                except Exception as _e:
-                    _r = _e.value
-                    break
-    else:
-        try:
-            if _s is None:
-                _y = next(_i)
-            else:
-                _y = _i.send(_s)
-        except StopIteration as _e:
-            _r = _e.value
-            break
-RESULT = _r
+# import sys
+#
+# RESULT = yield from EXPR
+# _i = iter(EXPR)
+# try:
+#     _y = next(_i)
+# except StopIteration as _e:
+#     _r = _e.value
+# else:
+#     while 1:
+#         try:
+#             _s = yield _y
+#         except GeneratorExit as _e:
+#             try:
+#                 _m = _i.close
+#             except AttributeError:
+#                 pass
+#             else:
+#                 _m()
+#             raise _e
+#         except BaseException as _e:
+#             _x = sys.exc_info()
+#             try:
+#                 _m = _i.throw
+#             except AttributeError:
+#                 raise _e
+#             else:
+#                 try:
+#                     _y = _m(*_x)
+#                 except Exception as _e:
+#                     _r = _e.value
+#                     break
+#     else:
+#         try:
+#             if _s is None:
+#                 _y = next(_i)
+#             else:
+#                 _y = _i.send(_s)
+#         except StopIteration as _e:
+#             _r = _e.value
+#             break
+# RESULT = _r
